8aacdcb0eb5378dad04133be1fbbd30b25c9ead9
Jakob Lass <lass.jakob@gmail.com>
2019-08-17 21:06:23 +0200

    Save all attributes in HISTORY when deleting from a table

--- testscript.py
+++ testscript.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python3 
 """
   Testing script
 """
@@ -9,7 +9,7 @@ import numpy as np
 
 mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
+    user="python",
     passwd="python",
     database = 'mydatabase'
 )
@@ -24,10 +24,10 @@ mycursor.execute("DROP TABLE HISTORYTABLES")
 
 
 mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
-
 mycursor.execute("SHOW TABLES")
-
 tables = [x[0] for x in mycursor]
+
+
 if not "HISTORY" in tables:
     mycursor.execute("CREATE TABLE HISTORY (id INT AUTO_INCREMENT PRIMARY KEY, table_id INT, "
     "action_name VARCHAR(255), action_values VARCHAR(255), action_conditions VARCHAR(255), action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
@@ -91,7 +91,7 @@ def revertCall(table_name,action_name,action_values,action_conditions,table_colu
             field, value = [x.strip() for x in pair.split(' = ')]
             fields.append(field.replace("'",'').replace('"',''))
             values.append(value)
-        print(['(',*[', '.join([x for x in fields])],')'])
+        #print(['(',*[', '.join([x for x in fields])],')'])
         fieldCall = ''.join(['(',*[', '.join([x for x in fields])],')'])
         valueCall = ''.join(['(',*[', '.join([x for x in values])],')'])
         revertCall = ''.join([revertCall,fieldCall,' VALUES ',valueCall])
@@ -119,22 +119,35 @@ def decorator(self,execute):
         
         if action in logActions and log == True:
             table = extractTable(call)
-            vals = extractValues(call,values)
+            
             conditions = extractCondition(call,values)
-
+            vals = extractValues(call,values)  
+            
             execute("SELECT id from HISTORYTABLES WHERE table_name = '{}'".format(table))
             tableID = self.fetchone()
             
             if tableID is None:
-                print('Table "{}" not found.'.format(table))
+                #print('Table "{}" not found.'.format(table))
                 execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{}'".format(table))
                 attributes = ', '.join([str(x[0]) for x in self.fetchall()])
-                print('Attributes:',attributes)
+                #print('Attributes:',attributes)
                 execute("INSERT INTO HISTORYTABLES (table_name,table_attributes) VALUES ('{}','{}')".format(table,attributes))
                 tableID = self.lastrowid
-                print('Inserting new with id {}'.format(tableID))
+                #print('Inserting new with id {}'.format(tableID))
             else:
                 tableID = tableID[0]
+            
+            if action == 'DELETE':
+                #print('Running:',"SELECT * from {} WHERE {}".format(table,conditions))
+                execute("SELECT * from {} WHERE {}".format(table,conditions))
+                vals = ', '.join(self.fetchone())
+                execute('SELECT table_attributes from HISTORYTABLES WHERE id = "{}"'.format(tableID))
+                attributes = self.fetchone()
+                print(attributes,vals)
+                conditions = ' AND '.join(['{} = "{}"'.format(a,v) for a,v in zip(attributes[0].split(', '),vals.split(', '))])
+                print(conditions)
+                vals = None
+
 
             historyCall = 'INSERT INTO HISTORY (table_id, action_name, action_values, action_conditions) VALUES (%s, %s, %s, %s)'
             execute(historyCall,(tableID,action,vals,conditions))
@@ -164,20 +177,20 @@ sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
 val = ('John', 'Highway 21')
 mycursor.execute(sql, val)
 
-sql = "DELETE from customers WHERE name = 'John'"
+sql = "DELETE from customers WHERE address = 'Highway 21'"
 
 mycursor.execute(sql)
 
 mycursor.execute("SHOW TABLES")
 tables = [x[0] for x in mycursor]
-print('\n\nDuring working:')
-print('Following tables exist',tables)
+#print('\n\nDuring working:')
+#print('Following tables exist',tables)
 
-for table in tables:
-    print('Contents of {}'.format(table))
-    mycursor.execute('SELECT * FROM {}'.format(table))
-    [print(x) for x in mycursor]
-print('\n\n')
+#for table in tables:
+#    print('Contents of {}'.format(table))
+#    mycursor.execute('SELECT * FROM {}'.format(table))
+#    [print(x) for x in mycursor]
+#print('\n\n')
 
 checkId = 2
 
