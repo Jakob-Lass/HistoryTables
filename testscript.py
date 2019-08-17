3b7245d9d61e0e3e11a4dfdd84f0ed57974f0d2b
Jakob Lass <lass.jakob@gmail.com>
2019-08-17 23:43:19 +0200

    Reorder and add/delete multiple

--- testscript.py
+++ testscript.py
@@ -7,53 +7,58 @@ import HistoryTable
 import numpy as np
 
 
-connection,mycursor = HistoryTable.connect(
+connection,myCursor = HistoryTable.connect(
     host="localhost",
     user="python",
     passwd="python",
     database = 'mydatabase'
 )
 
-mycursor.execute("SHOW TABLES")
-tables = [x[0] for x in mycursor]
+myCursor.execute("SHOW TABLES")
+tables = [x[0] for x in myCursor]
 
 if 'customers' in tables:
-    mycursor.execute("DROP TABLE customers")
-mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+    myCursor.execute("DROP TABLE customers")
 
+
+
+myCursor.execute("CREATE TABLE customers (name VARCHAR(255),address VARCHAR(255))")
+
+HistoryTable.printTables(myCursor)
 
 sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ('John', 'Highway 21')
-mycursor.execute(sql, val)
+
+
+myCursor.execute(sql, ('John', 'Highway 21'))
 
 if True:
     sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
     val = ('Jane', 'Highway 21')
-    mycursor.execute(sql, val)
-#HistoryTable.printTables(mycursor)
+    myCursor.execute(sql, val)
+    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+    val = ('Janice', 'Highway 21')
+    myCursor.execute(sql, val)
+    #HistoryTable.printTables(mycursor)
 
 
 sql = "DELETE from customers WHERE address = 'Highway 21'"
 
-mycursor.execute(sql)
+myCursor.execute(sql)
 
 
+HistoryTable.printTables(myCursor)
 
-checkId = 3
-
-HistoryTable.revertID(mycursor,checkId)
+checkId = 1
+HistoryTable.revertID(myCursor,checkId)
 
 checkId = 4
-HistoryTable.revertID(mycursor,checkId)
+HistoryTable.revertID(myCursor,checkId)
+
+#checkId = 4
+#HistoryTable.revertID(myCursor,checkId)
 
 
-HistoryTable.printTables(mycursor)
-
-
-
-#revertCall = 'DELETE from TABLE {} WHERE '.format(table_name)
-#testClause = ' AND '.join(['{} = {}'.format(column.strip(),value.strip()) for column,value in zip(table_columns.split(','),action_values.split(','))])
-
+HistoryTable.printTables(myCursor)
 
 #mydb.commit() # To commit actual changes
 
@@ -61,3 +66,4 @@ HistoryTable.printTables(mycursor)
 # CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))
 # ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY
 # INSERT INTO customers (name, address) VALUES (%s, %s)
+
